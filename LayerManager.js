e124f611d95e52e78d454592251135698117815d
Xhesi Balla <xhesiballa15@gmail.com>
2020-08-22 21:01:53 -0700

    Crop a layer

--- LayerManager.js
+++ LayerManager.js
@@ -1,9 +1,9 @@
 class LayerManager {
-    constructor(){
+    constructor() {
         this.layers = [];
     }
 
-    createNewLayerFromImage(image){
+    createNewLayerFromImage(image) {
         const layer = new Layer(500, 500);
         image.onload = () => {
             layer.drawImage(image);
@@ -12,45 +12,87 @@ class LayerManager {
         return layer;
     }
 
-    bringLayerToFront(layer){
-        this.layers.forEach(layer => layer.canvas.style.zIndex = 0);
-        layer.canvas.style.zIndex = 1;        
+    createEmptyLayer(){
+        return new Layer(500, 500);
+    }
+
+    addEmptyLayer() {
+        const layer = this.createEmptyLayer();
+        this.layers.push(layer);
+        return layer;
+    }
+
+    bringLayerToFront(layer) {
+        this.layers.forEach(layer => layer.canvas.parentElement.style.zIndex = 0);
+        layer.canvas.parentElement.style.zIndex = 1;
     }
 }
 
-class Layer{
-    constructor(height, width){
-        Object.assign(this, {height, width});
+class Layer {
+    constructor(height, width) {
+        Object.assign(this, { height, width });
         this.canvas = document.createElement('canvas');
         this.canvas.setAttribute("width", width);
         this.canvas.setAttribute("height", height);
     }
 
-    drawImage(image){
+    drawImage(image) {
         this.canvas.getContext('2d').drawImage(image, 0, 0, 500, 500);
     }
 
-    set opacity(opacity){
+    drawRectangle(x1, y1, x2, y2) {
+        const start = this.translate(x1, y1);
+        const end = this.translate(x2, y2);
+        const width = end.x - start.x;
+        const height = end.y - start.y;
+
+        this.clear();
         const ctx = this.canvas.getContext('2d');
-        const imageData = ctx.getImageData(0,0,this.width, this.height);
-        let index = 1;
-        while(index <= imageData.data.length){
-            if(index%4 === 0){
-                imageData.data[index-1] = opacity;
-            }
-            index++;
-        }
-        ctx.putImageData(imageData, 0, 0);
+        ctx.setLineDash([4, 2]);
+        ctx.strokeRect(start.x, start.y, width, height);
+    }
+
+    clear() {
+        this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
     getPixel(x, y) {
         let index = y * (this.width * 4) + x * 4;
-        const imageData = this.canvas.getContext('2d').getImageData(0,0,this.width, this.height);
+        const imageData = this.canvas.getContext('2d').getImageData(0, 0, this.width, this.height);
         return [
             imageData.data[index++],
             imageData.data[index++],
             imageData.data[index++],
             imageData.data[index++]
         ]
+    }
+
+    crop({ start, end }) {
+        const width = end.x - start.x;
+        const height = end.y - start.y;
+
+        const ctx = this.canvas.getContext('2d');
+        const imagedata = ctx.getImageData(start.x, start.y, width, height);
+        this.clear();
+        this.canvas.setAttribute("width", imagedata.width);
+        this.canvas.setAttribute("height", imagedata.height);
+        ctx.putImageData(imagedata, 0, 0);
+    }
+
+    translate(x, y) {
+        return { x: x * this.canvas.width / this.width, y: y * this.canvas.height / this.height }
+    }
+
+    set opacity(opacity) {
+        const ctx = this.canvas.getContext('2d');
+        const imageData = ctx.getImageData(0, 0, this.width, this.height);
+        let index = 1;
+        while (index <= imageData.data.length) {
+            if (index % 4 === 0) {
+                imageData.data[index - 1] = opacity;
+            }
+            index++;
+        }
+        ctx.putImageData(imageData, 0, 0);
     }
 }
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let layerManager  = null;
-const layerMap = {};
+let layerManager = null;
+let actionsLayer = null;
+let activeLayer = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-    layerManager= new LayerManager();
+    layerManager = new LayerManager();
+    actionsLayer = layerManager.createEmptyLayer();
+    actionsLayer.canvas.style.zIndex = 5;
+    showNewLayer(actionsLayer, false);
+    initializeActionsLayer(actionsLayer, false);
+    actionsLayer.canvas.parentElement.style.zIndex = 5;
 
     const input = document.querySelector('input[type=file]');
-    input.addEventListener("change", (event) => {
+    input.addEventListener('change', (event) => {
         const file = event.target.files[0];
         const fileReader = new FileReader();
 
@@ -16,16 +22,64 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         fileReader.readAsDataURL(file);
 
-        const layer = layerManager.createNewLayerFromImage(image);
-        document.getElementById('canvasContainer').append(layer.canvas);
+        showNewLayer(layerManager.createNewLayerFromImage(image));
+    })
 
+    const button = document.querySelector('input[type=button]');
+    button.addEventListener('click', crop);
+});
+
+function showNewLayer(layer, list = true) {
+    const layerContainer = document.createElement('div');
+    layerContainer.classList.add('layer');
+    layerContainer.append(layer.canvas);
+    document.getElementById('canvasContainer').append(layerContainer);
+    if (list) {
+        activeLayer = layer;
         const li = document.createElement('li');
         li.layer = layer;
-       
+
         li.innerText = `Layer ${layerManager.layers.length}`
         li.addEventListener('click', (event) => {
             layerManager.bringLayerToFront(layer);
+            activeLayer = layer;
         })
         document.getElementById('layers').append(li);
+    }
+}
+
+function initializeActionsLayer(layer) {
+    let start = null;
+    actionsLayer = layer;
+    layer.canvas.addEventListener('mousedown', (event) => {
+        const x = event.offsetX;
+        const y = event.offsetY;
+        start = { x, y };
+    });
+
+    layer.canvas.addEventListener('mouseup', (event) => {
+        start = null;
     })
-});
+
+    layer.canvas.addEventListener('mouseleave', (event) => {
+        start = null;
+    })
+
+    layer.canvas.addEventListener('mousemove', (event) => {
+        if (start) {
+            const x = event.offsetX;
+            const y = event.offsetY;
+            actionsLayer.drawRectangle(start.x, start.y, x, y);
+            layer.selection = { start, end: { x, y } }
+        }
+    })
+}
+
+function crop() {
+    if (activeLayer && actionsLayer.selection) {
+        console.log(activeLayer,actionsLayer.selection);
+        actionsLayer.clear();
+        activeLayer.crop(actionsLayer.selection);
+        actionsLayer.selection = null;
+    }
+}
