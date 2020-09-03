cf0902310dcfa8084336be4a80a86c0af4a4abfe
Xhesi Balla <xhesiballa15@gmail.com>
2020-09-03 00:55:42 -0700

    implement menu bar and new design

--- index.js
+++ index.js
@@ -27,41 +27,6 @@ document.addEventListener("DOMContentLoaded", () => {
         showNewLayer(layerManager.createNewLayerFromImage(image));
     })
 
-    const cropButton = document.getElementById('crop');
-    cropButton.addEventListener('click', (event) => {
-        event.target.blur();
-        const style = activeLayer.canvas.parentElement.style;
-        const _style = actionsLayer.canvas.parentElement.style
-        _style.top = style.top;
-        _style.left = style.left;
-        actionsLayer.canvas.getContext('2d').putImageData(new ImageData(activeLayer.canvas.width, activeLayer.canvas.height), 0, 0);
-        actionsLayer.canvas.setAttribute("width", activeLayer.canvas.width);
-        actionsLayer.canvas.setAttribute("height", activeLayer.canvas.height);
-        actionsLayer.canvas.parentElement.style.removeProperty('visibility');
-    });
-
-    const exportButton = document.getElementById('export');
-    exportButton.addEventListener('click', (event) => {
-        const {clientWidth, clientHeight} = document.getElementById('canvasContainer');
-        const canvas = document.createElement('canvas');
-        canvas.setAttribute('width', clientWidth);
-        canvas.setAttribute('height', clientHeight);
-        layerManager.layers.forEach(layer => {
-            console.log(layer.canvas.parentElement.style)
-            let {top, left} = layer.canvas.parentElement.style;
-            left = Number(left.substring(0, left.indexOf('px'))) || 0;
-            top = Number(top.substring(0, top.indexOf('px'))) || 0;
-            const data = layer.canvas.getContext('2d').getImageData(0,0,layer.canvas.width, layer.canvas.height);
-            canvas.getContext('2d').putImageData(data, left, top);
-        });
-        
-        const a = document.createElement("a");
-        a.href = canvas.toDataURL('image/jpeg');;
-        a.setAttribute("download", 'image.jpeg');
-        a.click();
-    });
-
-
     document.addEventListener('keydown', (event) => {
         if (event.key == "Enter") {
             crop();
@@ -82,6 +47,37 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+function exportImage(){
+    const {clientWidth, clientHeight} = document.getElementById('canvasContainer');
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('width', clientWidth);
+    canvas.setAttribute('height', clientHeight);
+    layerManager.layers.forEach(layer => {
+        let {top, left} = layer.canvas.parentElement.style;
+        left = Number(left.substring(0, left.indexOf('px'))) || 0;
+        top = Number(top.substring(0, top.indexOf('px'))) || 0;
+        const data = layer.canvas.getContext('2d').getImageData(0,0,layer.canvas.width, layer.canvas.height);
+        canvas.getContext('2d').putImageData(data, left, top);
+    });
+    
+    const a = document.createElement("a");
+    a.href = canvas.toDataURL('image/jpeg');;
+    a.setAttribute("download", 'image.jpeg');
+    a.click();
+}
+
+function startCrop(){
+    const style = activeLayer.canvas.parentElement.style;
+    const _style = actionsLayer.canvas.parentElement.style
+    actionsLayer.canvas.parentElement.classList.add('active');
+    _style.top = style.top;
+    _style.left = style.left;
+    actionsLayer.canvas.getContext('2d').putImageData(new ImageData(activeLayer.canvas.width, activeLayer.canvas.height), 0, 0);
+    actionsLayer.canvas.setAttribute("width", activeLayer.canvas.width);
+    actionsLayer.canvas.setAttribute("height", activeLayer.canvas.height);
+    actionsLayer.canvas.parentElement.style.removeProperty('visibility');
+}
 
 function showNewLayer(layer, list = true) {
     const layerContainer = document.createElement('div');
@@ -150,10 +146,15 @@ function initializeActionsLayer(layer) {
 
 function crop() {
     actionsLayer.canvas.parentElement.style.visibility = 'hidden';
+    actionsLayer.canvas.parentElement.style.removeProperty('active');
 
     if (activeLayer && actionsLayer.selection) {
         actionsLayer.clear();
         activeLayer.crop(actionsLayer.selection);
         actionsLayer.selection = null;
     }
+}
+
+function openFileUploadDialog(){
+    document.querySelector('input[type="file"]').click();
 }
